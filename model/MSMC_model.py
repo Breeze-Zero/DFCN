67b03c0558161974a77fd14e7e3fa870b3cc4b17
Breeze <78247190+Breeze-Zero@users.noreply.github.com>
2023-02-09 16:11:10 +0800

    Update MSMC_model.py

--- model/MSMC_model.py
+++ model/MSMC_model.py
@@ -1,13 +1,18 @@
+# Copyright (c) Breeze-Zero and fastMRI.
+
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+
 import math
 from typing import List, Optional, Tuple
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 import fastmri
 from fastmri.data import transforms
-
 from fastmri.models.unet import Unet
 
 
